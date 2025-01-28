2309184a848c21277c68a81c7c53c37b98daa494
fendnugraha <fendnugraha92@gmail.com>
2025-01-28 10:58:01 +0700

    tambah xsrf

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,13 +3,13 @@ import Axios from "axios";
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
-        "X-XSRF-TOKEN": localStorage.getItem("XSRF-TOKEN"),
         "X-Requested-With": "XMLHttpRequest",
         Accept: "application/json",
         "Content-Type": "application/json",
     },
     withCredentials: true,
     withXSRFToken: true,
+    xsrfCookieName: "XSRF-TOKEN",
 });
 
 export default axios;

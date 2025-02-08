0c9a4812bcd29ec5784bdc7b9a64b260410f602d
fendnugraha <fendnugraha92@gmail.com>
2025-02-08 19:04:38 +0700

    fix user role menu mobile

--- src/app/(app)/Header.js
+++ src/app/(app)/Header.js
@@ -23,6 +23,8 @@ const Header = ({ title }) => {
     const [isOpen, setIsOpen] = useState(false);
     const { profit, loading, error } = RankByProfit();
 
+    const pathname = usePathname();
+
     const userWarehouseId = user?.role?.warehouse_id;
     const userWarehouseName = user?.role?.warehouse?.name;
     const WarehouseRank = profit?.data?.findIndex((item) => Number(item.warehouse_id) === userWarehouseId) + 1 || 0;
@@ -59,40 +61,48 @@ const Header = ({ title }) => {
                     isOpen ? "opacity-100 scale-y-100 h-auto" : "opacity-0 scale-y-0 h-0"
                 } border-b origin-top sm:hidden sm:bg-none bg-white`}
             >
-                <ul className="space-y-2">
+                <div className="flex justify-between py-2 bg-indigo-400 text-white items-center gap-2 px-4">
+                    <h1 className="text-md font-bold">{user.role?.warehouse?.name}</h1>
+                    <span className="text-sm">{user.role?.role}</span>
+                </div>
+                <ul className="space-y-2 pt-4">
                     <li className="">
-                        <ResponsiveNavLink href="/dashboard" active={usePathname() === "/dashboard"}>
+                        <ResponsiveNavLink href="/dashboard" active={pathname === "/dashboard"}>
                             <LayoutDashboardIcon size={20} className="mr-2 inline" /> Dashboard
                         </ResponsiveNavLink>
                     </li>
                     <li className="">
-                        <ResponsiveNavLink href="/transaction" active={usePathname() === "/transaction"}>
+                        <ResponsiveNavLink href="/transaction" active={pathname === "/transaction"}>
                             <ArrowRightLeftIcon size={20} className="mr-2 inline" /> Transaction
                         </ResponsiveNavLink>
                     </li>
                     <li className="">
-                        <ResponsiveNavLink href="/store" active={usePathname() === "/store"}>
+                        <ResponsiveNavLink href="/store" active={pathname === "/store"}>
                             <StoreIcon size={20} className="mr-2 inline" /> Store
                         </ResponsiveNavLink>
                     </li>
-                    <li className="">
-                        <ResponsiveNavLink href="/finance" active={usePathname() === "/finance"}>
-                            <CircleDollarSignIcon size={20} className="mr-2 inline" /> Finance
-                        </ResponsiveNavLink>
-                    </li>
-                    <li className="">
-                        <ResponsiveNavLink href="/summary" active={usePathname() === "/summary"}>
-                            <ChartAreaIcon size={20} className="mr-2 inline" /> Summary
-                        </ResponsiveNavLink>
-                    </li>
-                    <li className="border-t py-2">
-                        <ResponsiveNavLink href="/setting" active={usePathname().startsWith("/setting")}>
-                            <CogIcon size={20} className="mr-2 inline" /> Setting
-                        </ResponsiveNavLink>
-                    </li>
+                    {user.role?.role === "Administrator" && (
+                        <>
+                            <li className="">
+                                <ResponsiveNavLink href="/finance" active={pathname === "/finance"}>
+                                    <CircleDollarSignIcon size={20} className="mr-2 inline" /> Finance
+                                </ResponsiveNavLink>
+                            </li>
+                            <li className="">
+                                <ResponsiveNavLink href="/summary" active={pathname === "/summary"}>
+                                    <ChartAreaIcon size={20} className="mr-2 inline" /> Summary
+                                </ResponsiveNavLink>
+                            </li>
+                            <li className="border-t py-2">
+                                <ResponsiveNavLink href="/setting" active={pathname.startsWith("/setting")}>
+                                    <CogIcon size={20} className="mr-2 inline" /> Setting
+                                </ResponsiveNavLink>
+                            </li>
+                        </>
+                    )}
                     <li className="border-t py-4">
                         <ResponsiveNavButton onClick={logout}>
-                            <CirclePowerIcon size={20} className="mr-2 inline" /> Logout
+                            <CirclePowerIcon size={20} className="mr-2 inline" /> Logout ({user.email})
                         </ResponsiveNavButton>
                     </li>
                 </ul>

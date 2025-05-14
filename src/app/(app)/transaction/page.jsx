b0536fbc5336631805b819ea97710e83d4197934
fendnugraha <fend@fendnugrahas-MacBook-Air.local>
2025-05-14 16:15:19 +0700

    cek konsol

--- src/app/(app)/transaction/page.jsx
+++ src/app/(app)/transaction/page.jsx
@@ -196,7 +196,7 @@ const TransactionPage = () => {
             dailyDashboard?.data?.totalCash > openingCash ? totalSetoran - openingCash : totalSetoran
         )}`;
     };
-
+    console.log(cashBank);
     return (
         <>
             <Header title="Transaction" />

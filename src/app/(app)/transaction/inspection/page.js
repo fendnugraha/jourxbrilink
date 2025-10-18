63cba9fc720bed05175686f4031fab5269a211cb
Fend Nugraha <fendnugraha@Fends-Book.local>
2025-10-18 10:31:59 +0700

    fix loading

--- src/app/(app)/transaction/inspection/page.js
+++ src/app/(app)/transaction/inspection/page.js
@@ -181,7 +181,7 @@ const InspectionPage = () => {
             <MainPage headerTitle="Inspection">
                 <div className="grid grid-cols-4 gap-4 p-4 sm:p-8">
                     <div className="card p-4 col-span-3 relative">
-                        {!loading && (
+                        {loading && (
                             <div className="absolute top-24 left-3 flex items-center gap-2">
                                 <Loader2 size={16} className="animate-spin" /> <span className="italic text-xs">Loading data...</span>
                             </div>

5fcdcf7ff456e5524f2f6815425cda42e46481c2
Fend Nugraha <fendnugraha@Fends-Book.local>
2025-11-28 17:30:26 +0700

    tambah bounce button location fix config

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
             },
             {
                 protocol: "https",
-                hostname: "api.threekomunika.com",
+                hostname: "api.three-komunika.com",
                 pathname: "/storage/**",
             },
         ],

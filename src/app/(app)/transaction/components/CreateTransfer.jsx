38452ce4839d62dcfb40678820a19228251e8ce7
Fend Nugraha <fendnugraha@Fends-Book.local>
2025-12-03 10:41:07 +0700

    fix form-control

--- src/app/(app)/transaction/components/CreateTransfer.jsx
+++ src/app/(app)/transaction/components/CreateTransfer.jsx
@@ -141,7 +141,7 @@ const CreateTransfer = ({
                         </Label>
                         <div className="">
                             <input
-                                className={`form-control ${feeAdminAuto ? "!bg-green-200 text-green-700 font-bold" : ""}`}
+                                className={`form-control ${feeAdminAuto ? "!bg-green-200 !text-green-700 font-bold" : ""}`}
                                 type="number"
                                 placeholder={feeAdminAuto ? "Rp. (Autofilled)" : "Rp."}
                                 value={formData.fee_amount}

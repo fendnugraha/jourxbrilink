346abb7074a51599b2026323b837ef94b290907e
Fend Nugraha <fendnugraha@Fends-Book.local>
2025-12-16 20:47:03 +0700

    display rcv

--- src/app/(app)/employee/attendance/AttendanceCalendar.jsx
+++ src/app/(app)/employee/attendance/AttendanceCalendar.jsx
@@ -2,7 +2,7 @@
 
 import Calendar from "@/components/Calendar";
 import axios from "@/libs/axios";
-import { DateTimeNow } from "@/libs/format";
+import { DateTimeNow, formatNumber } from "@/libs/format";
 import { AlarmClockPlus, Clock, IdCardLanyard, Star, User } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -30,6 +30,8 @@ const AttendanceCalendar = () => {
         fetchAttendanceData();
     }, [selectedDate, employeeId]);
 
+    console.log(attendanceData);
+
     const calendarData = {};
 
     attendanceData.forEach((item) => {
@@ -46,19 +48,38 @@ const AttendanceCalendar = () => {
 
     return (
         <div>
-            <div className="px-4 flex gap-2">
-                <span className="flex gap-1 items-center w-fit justify-between bg-slate-500 text-white rounded-full px-3 py-0.5">
-                    <IdCardLanyard size={15} strokeWidth={2} /> {attendanceData?.[0]?.contact?.name ?? "N/A"}
-                </span>
-                <span className="flex gap-1 items-center w-16 justify-between bg-yellow-300 text-yellow-800 rounded-full px-2 py-0.5">
-                    <Star size={15} strokeWidth={2} fill="orange" /> {attGood}
-                </span>
-                <span className="flex gap-1 items-center w-16 justify-between bg-violet-300 text-violet-800 rounded-full px-2 py-0.5">
-                    <AlarmClockPlus size={15} strokeWidth={2} fill="orange" /> {overtime}
-                </span>
-                <span className="flex gap-1 items-center w-16 justify-between bg-red-500 text-white rounded-full px-2 py-0.5">
-                    <Clock size={15} /> {attLate}
-                </span>
+            <div className="px-4 flex justify-between flex-col sm:flex-row gap-2">
+                <div className="flex gap-2 flex-col sm:flex-row">
+                    <span className="flex gap-1 items-center w-fit justify-between bg-slate-500 text-white rounded-full px-3 py-0.5">
+                        <IdCardLanyard size={15} strokeWidth={2} /> {attendanceData?.[0]?.contact?.name ?? "N/A"}
+                    </span>
+                    <div className="flex gap-2">
+                        <span className="flex gap-1 items-center w-full sm:w-16 justify-between bg-yellow-300 text-yellow-800 rounded-full px-2 py-0.5">
+                            <Star size={15} strokeWidth={2} fill="orange" /> {attGood}
+                        </span>
+                        <span className="flex gap-1 items-center w-full sm:w-16 justify-between bg-violet-300 text-violet-800 rounded-full px-2 py-0.5">
+                            <AlarmClockPlus size={15} strokeWidth={2} fill="orange" /> {overtime}
+                        </span>
+                        <span className="flex gap-1 items-center w-full sm:w-16 justify-between bg-red-500 text-white rounded-full px-2 py-0.5">
+                            <Clock size={15} /> {attLate}
+                        </span>
+                    </div>
+                </div>
+                {(attendanceData?.[0]?.contact?.employee_receivables_sum || attendanceData?.[0]?.contact?.installment_receivables_sum) && (
+                    <div className="flex gap-2 items-center justify-between border text-sm border-slate-300 rounded-full px-2 py-1">
+                        {attendanceData?.[0]?.contact?.employee_receivables_sum && (
+                            <h1>
+                                Kasbon: Rp <span className="font-bold">{formatNumber(attendanceData?.[0]?.contact?.employee_receivables_sum?.total) ?? 0}</span>
+                            </h1>
+                        )}
+                        {attendanceData?.[0]?.contact?.installment_receivables_sum && (
+                            <h1>
+                                Cicilan: Rp{" "}
+                                <span className="font-bold">{formatNumber(attendanceData?.[0]?.contact?.installment_receivables_sum?.total) ?? 0}</span>
+                            </h1>
+                        )}
+                    </div>
+                )}
             </div>
             <Calendar data={calendarData} withHeader={false} maxWidth="sm:max-w-full" />
         </div>

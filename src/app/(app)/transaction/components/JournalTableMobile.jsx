d554ae8949181a938b555bdfd81aa944090dd328
fend nugraha <fendnugraha@fends-MacBook-Air.local>
2026-03-11 21:02:48 +0700

    fix mutasi mobile

--- src/app/(app)/transaction/components/JournalTableMobile.jsx
+++ src/app/(app)/transaction/components/JournalTableMobile.jsx
@@ -28,7 +28,7 @@ export const JournalTableMobile = ({
         <div className="px-2 w-full space-y-2">
             {currentItems.map((journal, index) => (
                 <div key={index} className="bg-slate-100 dark:bg-slate-700 flex justify-between items-center px-3 py-2 rounded-lg">
-                    <div className="font-bold text-xs block text-lime-600 dark:text-lime-300 group-hover:text-lime-700 group-hover:dark:text-lime-400">
+                    <div className="font-bold text-xs block group-hover:text-lime-700 group-hover:dark:text-lime-400">
                         <span className="text-xs text-blue-700 dark:text-blue-300 group-hover:dark:text-blue-200 group-hover:text-blue-400 block">
                             {formatDateTime(journal.date_issued)}
                         </span>
@@ -44,13 +44,16 @@ export const JournalTableMobile = ({
                             <>
                                 {journal.cred?.account_group}{" "}
                                 {journal.cred?.warehouse?.id !== warehouse && (
-                                    <span className="text-slate-500 dark:text-slate-300">({journal.cred?.warehouse?.code})</span>
+                                    <span className="text-yellow-600 dark:text-yellow-300">({journal.cred?.warehouse?.code})</span>
                                 )}
                                 <span className="block ml-1">
-                                    {" → "}
-                                    {journal.debt?.account_group}{" "}
+                                    <span className="text-green-500 dark:text-green-300">
+                                        {" "}
+                                        {" → "}
+                                        {journal.debt?.account_group}
+                                    </span>{" "}
                                     {journal.debt?.warehouse?.id !== warehouse && (
-                                        <span className="text-slate-500 dark:text-slate-300">({journal.debt?.warehouse?.code})</span>
+                                        <span className="text-yellow-600 dark:text-yellow-300">({journal.debt?.warehouse?.code})</span>
                                     )}
                                 </span>
                             </>

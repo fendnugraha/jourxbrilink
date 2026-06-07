035976157d788bac9c472848b6fb8aecbf5cd576
fend nugraha <fendnugraha@fends-MacBook-Air.local>
2026-06-07 19:14:31 +0700

    add zone warehouse balance

--- src/app/(app)/summary/components/WarehouseBalance.jsx
+++ src/app/(app)/summary/components/WarehouseBalance.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import formatNumber from "@/libs/formatNumber";
 import { DownloadIcon, FilterIcon, RefreshCcwIcon, Star } from "lucide-react";
 import Input from "@/components/Input";
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { getStorePerformanceRating } from "@/libs/GetStorePerformanceRating";
 import { useGetWarehouseBalance } from "@/libs/getWarehouseBalance";
 import { TimeAgo } from "@/libs/format";
+import axios from "@/libs/axios";
 
 const getCurrentDate = () => {
     const today = new Date();
@@ -24,6 +25,21 @@ const WarehouseBalance = () => {
     const [endDate, setEndDate] = useState(getCurrentDate());
     const { warehouseBalance, warehouseBalanceError, isValidating, mutate } = useGetWarehouseBalance(endDate);
     const [isModalFilterDataOpen, setIsModalFilterDataOpen] = useState(false);
+
+    const [zones, setZones] = useState([]);
+    const [selectedZone, setSelectedZone] = useState("");
+    const fetchZones = useCallback(async () => {
+        try {
+            const response = await axios.get("/api/zones");
+            setZones(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchZones();
+    }, [fetchZones]);
 
     const closeModal = () => {
         setIsModalFilterDataOpen(false);
@@ -50,13 +66,21 @@ const WarehouseBalance = () => {
                     <span className="card-subtitle">Periode: {endDate}</span>
                 </h4>
                 <div className="flex gap-1 h-fit">
+                    <select className="form-select !w-96" value={selectedZone} onChange={(e) => setSelectedZone(e.target.value)}>
+                        <option value="">Semua Zona</option>
+                        {zones?.map((zone) => (
+                            <option key={zone?.id} value={zone?.id}>
+                                {zone?.zone_name}
+                            </option>
+                        ))}
+                    </select>
                     <button onClick={() => mutate()} className="small-button">
                         <RefreshCcwIcon className="size-4" />
                     </button>
                     <button className="small-button" disabled={true}>
                         <DownloadIcon className="size-4" />
                     </button>
-                    <Input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} className="form-control " />
+                    <Input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} className="form-control !w-fit" />
                     {/* <button onClick={() => setIsModalFilterDataOpen(true)} className="small-button">
                         <FilterIcon className="size-4" />
                     </button> */}
@@ -80,47 +104,52 @@ const WarehouseBalance = () => {
                                 <td colSpan={4}>Loading...</td>
                             </tr>
                         ) : (
-                            warehouseBalance.warehouse?.map((w, i) => (
-                                <tr key={i}>
-                                    <td className="">
-                                        <Link className="hover:underline" href={`/summary/warehouse/${w.id}`}>
-                                            {i + 1}. {w.name.replace(/^konter\s*/i, "")}
-                                        </Link>
-                                    </td>
-                                    <td className="w-25">
-                                        {(() => {
-                                            const percent = w.id !== 1 && calculateLimitPercentage(w.total_cash_limit, w.cash);
-                                            const color = getLimitColor(percent);
+                            warehouseBalance.warehouse
+                                ?.filter((w) => selectedZone === "" || w.zone_id === Number(selectedZone))
+                                .map((w, i) => (
+                                    <tr key={i}>
+                                        <td className="">
+                                            <Link className="hover:underline" href={`/summary/warehouse/${w.id}`}>
+                                                {i + 1}. {w.name.replace(/^konter\s*/i, "")}
+                                            </Link>
+                                        </td>
+                                        <td className="w-25">
+                                            {(() => {
+                                                const percent = w.id !== 1 && calculateLimitPercentage(w.total_cash_limit, w.cash);
+                                                const color = getLimitColor(percent);
 
-                                            return (
-                                                <div>
-                                                    {w.id !== 1 && (
-                                                        <div className="bg-slate-200 dark:bg-slate-700 rounded h-0.5 w-full overflow-hidden">
-                                                            <div className={`${color} h-0.5 transition-all duration-500`} style={{ width: `${percent}%` }} />
+                                                return (
+                                                    <div>
+                                                        {w.id !== 1 && (
+                                                            <div className="bg-slate-200 dark:bg-slate-700 rounded h-0.5 w-full overflow-hidden">
+                                                                <div
+                                                                    className={`${color} h-0.5 transition-all duration-500`}
+                                                                    style={{ width: `${percent}%` }}
+                                                                />
+                                                            </div>
+                                                        )}
+
+                                                        <div className="flex justify-between text-xs mt-1">
+                                                            <span>{formatNumber(w.cash)}</span>
                                                         </div>
-                                                    )}
-
-                                                    <div className="flex justify-between text-xs mt-1">
-                                                        <span>{formatNumber(w.cash)}</span>
                                                     </div>
-                                                </div>
-                                            );
-                                        })()}
-                                    </td>
-                                    <td className="text-end">{formatNumber(w.bank)}</td>
-                                    <td
-                                        className={`text-end font-bold ${w.cash + w.bank - w.total_limit !== 0 && w.id !== 1 && "text-red-500 dark:text-red-400"}`}
-                                    >
-                                        {formatNumber(w.cash + w.bank)}
-                                    </td>
-                                    <td className="text-center text-slate-500">{<TimeAgo timestamp={w.updated_at} />}</td>
-                                    <td className="text-center w-12">
-                                        {w.id > 1 && (
-                                            <span className="bg-green-300 text-green-800 p-1 rounded">{getStorePerformanceRating(w.average_profit)}</span>
-                                        )}
-                                    </td>
-                                </tr>
-                            ))
+                                                );
+                                            })()}
+                                        </td>
+                                        <td className="text-end">{formatNumber(w.bank)}</td>
+                                        <td
+                                            className={`text-end font-bold ${w.cash + w.bank - w.total_limit !== 0 && w.id !== 1 && "text-red-500 dark:text-red-400"}`}
+                                        >
+                                            {formatNumber(w.cash + w.bank)}
+                                        </td>
+                                        <td className="text-center text-slate-500">{<TimeAgo timestamp={w.updated_at} />}</td>
+                                        <td className="text-center w-12">
+                                            {w.id > 1 && (
+                                                <span className="bg-green-300 text-green-800 p-1 rounded">{getStorePerformanceRating(w.average_profit)}</span>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))
                         )}
                     </tbody>
                     <tfoot>

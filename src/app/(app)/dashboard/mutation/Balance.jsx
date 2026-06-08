9e50fec90c65247560a3bd1a1910c7e28f513310
fend nugraha <fendnugraha@fends-MacBook-Air.local>
2026-06-08 12:44:31 +0700

    fix balance

--- src/app/(app)/dashboard/mutation/Balance.jsx
+++ src/app/(app)/dashboard/mutation/Balance.jsx
@@ -1,7 +1,9 @@
 import { formatNumber } from "@/libs/format";
 import { ArrowDown, ArrowUp, Landmark } from "lucide-react";
+import { useState } from "react";
 
 const Balance = ({ accountBalance, journalsByWarehouse }) => {
+    const [showAccountName, setShowAccountName] = useState(false);
     const mutationInSumById = (acc_id) => {
         return journalsByWarehouse?.data?.reduce(
             (sum, journal) => (Number(journal.debt_code) === Number(acc_id) && journal.trx_type === "Mutasi Kas" ? sum + Number(journal.amount) : sum),
@@ -22,14 +24,16 @@ const Balance = ({ accountBalance, journalsByWarehouse }) => {
     return (
         <div className="space-y-2">
             {accountBalance?.data?.chartOfAccounts?.map((account) => (
-                <div className="card p-3" key={account?.id} hidden={account?.balance === 0}>
+                <div className="bg-white dark:bg-slate-700/90 rounded-3xl p-3" key={account?.id} hidden={account?.balance === 0}>
                     <div className="flex justify-between items-start gap-4">
                         <span className="flex items-center justify-center gap-2 bg-amber-500 dark:bg-amber-600 text-white rounded-full w-8 h-8">
                             <Landmark size={16} />
                         </span>
                         <div className="flex flex-col justify-end items-end overflow-x-hidden flex-1">
-                            {/* <h1 className="text-xs font-bold text-nowrap">{account?.account_group}</h1> */}
-                            <h1 className="text-xs text-nowrap text-green-600 dark:text-green-300">{account?.acc_name}</h1>
+                            <h1 className="text-xs text-nowrap text-green-600 dark:text-green-300" onClick={() => setShowAccountName(!showAccountName)}>
+                                {showAccountName ? <span className="font-bold">{account?.account_group}</span> : account?.acc_name}
+                            </h1>
+                            {/* <h1 className="text-[0.6rem] text-nowrap">{account?.acc_name}</h1> */}
                             <h1 className="text-md font-semibold text-right">{formatNumber(account?.balance)}</h1>
                             <div className="flex w-full justify-between bg-gray-300/50 dark:bg-gray-300/20 mt-2 py-1 px-2 rounded-lg">
                                 <div className="flex items-center gap-1">

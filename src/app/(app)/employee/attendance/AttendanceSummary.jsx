4d6d927cb40b969004e318c01cbc42c75ce15bed
fend nugraha <fendnugraha@fends-MacBook-Air.local>
2026-06-22 16:16:46 +0700

    add podium

--- src/app/(app)/employee/attendance/AttendanceSummary.jsx
+++ src/app/(app)/employee/attendance/AttendanceSummary.jsx
@@ -1,7 +1,8 @@
 import axios from "@/libs/axios";
 import { formatNumber, toOrdinal } from "@/libs/format";
-import { ArrowBigDown, ArrowBigUp, ChartNoAxesColumn, ChessQueen, Clock, Gift, Star } from "lucide-react";
+import { ArrowBigDown, ArrowBigUp, ChartNoAxesColumn, ChessQueen, Clock, Gift, Grid, List, Star } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
+import AttendanceSummaryList from "./AttendanceSummaryList";
 
 const AttendanceSummary = ({ dateString, selectedZone }) => {
     const [search, setSearch] = useState("");
@@ -10,6 +11,7 @@ const AttendanceSummary = ({ dateString, selectedZone }) => {
     const month = date.getMonth() + 1; // 1–12
     const year = date.getFullYear();
     const [employees, setEmployees] = useState([]);
+    const [listMode, setListMode] = useState(false);
 
     const fetchEmployees = useCallback(async ({ month = null, year = null } = {}) => {
         try {
@@ -36,72 +38,80 @@ const AttendanceSummary = ({ dateString, selectedZone }) => {
         }
     };
 
-    console.log(employees);
-
     return (
         <>
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 justify-between">
                 {/* <button className="small-button">
                     <ChartNoAxesColumn strokeWidth={2} size={18} />
                 </button> */}
                 <input type="search" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} className="form-control !w-1/2" />
+                <button className="small-button" onClick={() => setListMode(!listMode)}>
+                    {listMode ? <List strokeWidth={2} size={18} /> : <Grid strokeWidth={2} size={18} />}
+                </button>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-5 gap-4">
-                {employees
-                    .filter((employee) => {
-                        const matchSearch = employee.contact?.name?.toLowerCase().includes(search.toLowerCase());
+            {listMode ? (
+                <AttendanceSummaryList employees={employees} search={search} selectedZone={selectedZone} checkUpOrDown={checkUpOrDown} />
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-5 gap-4">
+                    {employees
+                        .filter((employee) => {
+                            const matchSearch = employee.contact?.name?.toLowerCase().includes(search.toLowerCase());
 
-                        const matchZone =
-                            selectedZone === "" ||
-                            Number(employee.warehouse?.warehouse_zone_id) === Number(selectedZone) ||
-                            Number(employee.contact?.zone?.id) === Number(selectedZone);
+                            const matchZone =
+                                selectedZone === "" ||
+                                Number(employee.warehouse?.warehouse_zone_id) === Number(selectedZone) ||
+                                Number(employee.contact?.zone?.id) === Number(selectedZone);
 
-                        const hasRating = (employee.attendance_rating?.rating ?? 0) > 0;
+                            const hasRating = (employee.attendance_rating?.rating ?? 0) > 0;
 
-                        return matchSearch && matchZone && hasRating;
-                    })
-                    .sort((a, b) => b.attendance_rating?.rating - a.attendance_rating?.rating)
-                    .map((employee, index) => (
-                        <div key={employee.id} className="group p-4 border drop-shadow-sm border-slate-200 dark:bg-slate-800 dark:border-slate-700 rounded-4xl">
-                            <div className="flex flex-col items-center gap-2 mt-4">
-                                <span className="text-5xl font-black mb-1">
-                                    {formatNumber(employee.attendance_rating?.rating ?? 0)}
-                                    <sub className="font-normal text-xs">/10</sub>
-                                </span>
-                                <h2 className="font-semibold text-center mb-4 text-sm">
-                                    {employee.contact?.name}
-                                    <span className="block text-xs font-normal">Total Score: {formatNumber(employee.attendance_rating?.score ?? 0)}</span>
-                                </h2>
-                                {checkUpOrDown(employee.attendance_rating?.rating ?? 0, employee.attendance_rating_last_month?.rating ?? 0)}
-                                <div className="absolute top-4 right-4">
-                                    <Gift
-                                        fill={employee.attendance_rating?.good > 0 && employee.attendance_rating?.late === 0 ? "yellow" : "none"}
-                                        strokeWidth={2}
-                                        className={`${employee.attendance_rating?.good > 0 && employee.attendance_rating?.late === 0 ? "text-yellow-600" : "text-slate-300 dark:text-slate-600"} text-orange-600 group-hover:rotate-12 transform duration-300`}
-                                        size={24}
-                                    />
-                                </div>
-                                <h1 className="absolute top-4 left-4 font-bold">
-                                    {index + 1 === 1 ? <ChessQueen size={36} fill="yellow" /> : <>{toOrdinal(index + 1)}</>}
-                                </h1>
-                                <div className="flex gap-4 w-full">
-                                    <div className="flex gap-1 justify-center items-center">
-                                        <div>
-                                            <Star fill="orange" strokeWidth={2} className="text-yellow-600" size={20} />
-                                        </div>
-                                        <div className="font-medium">{employee.attendance_rating?.good ?? 0}</div>
+                            return matchSearch && matchZone && hasRating;
+                        })
+                        .sort((a, b) => b.attendance_rating?.rating - a.attendance_rating?.rating)
+                        .map((employee, index) => (
+                            <div
+                                key={employee.id}
+                                className="group p-4 border drop-shadow-sm border-slate-200 dark:bg-slate-800 dark:border-slate-700 rounded-4xl"
+                            >
+                                <div className="flex flex-col items-center gap-2 mt-4">
+                                    <span className="text-5xl font-black mb-1">
+                                        {formatNumber(employee.attendance_rating?.rating ?? 0)}
+                                        <sub className="font-normal text-xs">/10</sub>
+                                    </span>
+                                    <h2 className="font-semibold text-center mb-4 text-sm">
+                                        {employee.contact?.name}
+                                        <span className="block text-xs font-normal">Total Score: {formatNumber(employee.attendance_rating?.score ?? 0)}</span>
+                                    </h2>
+                                    {checkUpOrDown(employee.attendance_rating?.rating ?? 0, employee.attendance_rating_last_month?.rating ?? 0)}
+                                    <div className="absolute top-4 right-4">
+                                        <Gift
+                                            fill={employee.attendance_rating?.good > 0 && employee.attendance_rating?.late === 0 ? "yellow" : "none"}
+                                            strokeWidth={2}
+                                            className={`${employee.attendance_rating?.good > 0 && employee.attendance_rating?.late === 0 ? "text-yellow-600" : "text-slate-300 dark:text-slate-600"} text-orange-600 group-hover:rotate-12 transform duration-300`}
+                                            size={24}
+                                        />
                                     </div>
-                                    <div className="flex gap-1 justify-center items-center">
-                                        <div>
-                                            <Clock fill="red" strokeWidth={2} className="text-red-300" size={20} />
+                                    <h1 className="absolute top-4 left-4 font-bold">
+                                        {index + 1 === 1 ? <ChessQueen size={36} fill="yellow" /> : <>{toOrdinal(index + 1)}</>}
+                                    </h1>
+                                    <div className="flex gap-4 w-full">
+                                        <div className="flex gap-1 justify-center items-center">
+                                            <div>
+                                                <Star fill="orange" strokeWidth={2} className="text-yellow-600" size={20} />
+                                            </div>
+                                            <div className="font-medium">{employee.attendance_rating?.good ?? 0}</div>
                                         </div>
-                                        <div className="font-medium">{employee.attendance_rating?.late ?? 0}</div>
+                                        <div className="flex gap-1 justify-center items-center">
+                                            <div>
+                                                <Clock fill="red" strokeWidth={2} className="text-red-300" size={20} />
+                                            </div>
+                                            <div className="font-medium">{employee.attendance_rating?.late ?? 0}</div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-            </div>
+                        ))}
+                </div>
+            )}
         </>
     );
 };
---
+++ src/app/(app)/employee/attendance/AttendanceSummaryList.jsx
@@ -0,0 +1,129 @@
+import { toOrdinal } from "@/libs/format";
+import { ArrowBigDown, ArrowBigUp, ChessQueen, Clock, Crown, Medal, Star, Trophy } from "lucide-react";
+
+const AttendanceSummaryList = ({ employees, search, selectedZone }) => {
+    const checkUpOrDown = (rating, lastRating) => {
+        if (rating > lastRating) {
+            return <ArrowBigUp fill="green" strokeWidth={2} className="text-green-600" size={20} />;
+        } else if (rating < lastRating) {
+            return <ArrowBigDown fill="red" strokeWidth={2} className="text-red-600" size={20} />;
+        } else {
+            return "";
+        }
+    };
+
+    const filteredSortedEmployees = employees
+        ?.filter((employee) => {
+            const matchSearch = employee.contact?.name?.toLowerCase().includes(search.toLowerCase());
+
+            const matchZone =
+                selectedZone === "" ||
+                Number(employee.warehouse?.warehouse_zone_id) === Number(selectedZone) ||
+                Number(employee.contact?.zone?.id) === Number(selectedZone);
+
+            return matchSearch && matchZone;
+        })
+        .sort((a, b) => b.attendance_rating?.rating - a.attendance_rating?.rating);
+
+    if (filteredSortedEmployees?.length === 0) {
+        return <div className="p-4">No employee found</div>;
+    }
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+            <table className="table-auto table w-full text-sm">
+                <thead>
+                    <tr>
+                        <th>Pos.</th>
+                        <th>Name</th>
+                        <th>
+                            <Star fill="orange" strokeWidth={2} className="text-yellow-600" size={20} />
+                        </th>
+                        <th>
+                            <Clock fill="red" strokeWidth={2} className="text-red-300" size={20} />
+                        </th>
+                        <th colSpan={2}>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredSortedEmployees?.map((employee, index) => {
+                        return (
+                            <tr key={employee?.id}>
+                                <td>{index + 1 === 1 ? <ChessQueen size={20} fill="yellow" className="text-yellow-600" /> : <>{toOrdinal(index + 1)}</>}</td>
+                                <td>{employee?.contact?.name}</td>
+                                <td className="text-center">{employee.attendance_rating?.good ?? 0}</td>
+                                <td className="text-center">{employee.attendance_rating?.late ?? 0}</td>
+                                <td className="text-center font-bold">
+                                    {employee.attendance_rating?.rating ?? 0}
+                                    <sub className="font-normal">/10</sub>
+                                </td>
+                                <td className="text-center">
+                                    {checkUpOrDown(employee.attendance_rating?.rating ?? 0, employee.attendance_rating_last_month?.rating ?? 0)}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+            <div className="p-4">
+                <div className="grid grid-cols-3 gap-2 items-end">
+                    <div className="flex flex-col items-center justify-center bg-amber-400 dark:bg-sky-900 rounded-t-3xl h-64">
+                        <h1 className="font-black">2nd</h1>
+                        <Medal size={50} fill="yellow" className="dark:text-yellow-600" />
+                        <span className="text-3xl font-bold">{filteredSortedEmployees?.[1]?.attendance_rating?.rating ?? 0}</span>
+                        <h1 className="font-bold bg-amber-600 dark:bg-sky-700 text-xs px-2 rounded-full text-white my-2">
+                            {filteredSortedEmployees?.[1]?.contact?.name ?? ""}
+                        </h1>
+                        <div className="flex items-center justify-evenly w-full gap-1">
+                            <div className="flex flex-col items-center gap-1 font-bold">
+                                <Star fill="orange" strokeWidth={2} className="text-yellow-100 dark:text-yellow-300" size={30} />
+                                {filteredSortedEmployees?.[1]?.attendance_rating?.good ?? 0}
+                            </div>
+                            <div className="flex flex-col items-center gap-1 font-bold">
+                                <Clock fill="red" strokeWidth={2} className="text-red-100" size={30} />
+                                {filteredSortedEmployees?.[1]?.attendance_rating?.late ?? 0}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center bg-amber-400 dark:bg-sky-900 rounded-t-3xl h-96">
+                        <h1 className="font-black">1st</h1>
+                        <Trophy size={100} fill="yellow" className="dark:text-yellow-600" />
+                        <span className="text-4xl font-bold">{filteredSortedEmployees?.[0]?.attendance_rating?.rating ?? 0}</span>
+                        <h1 className="font-bold bg-amber-600 dark:bg-sky-700 text-sm px-2 rounded-full text-white my-2">
+                            {filteredSortedEmployees?.[0]?.contact?.name ?? ""}
+                        </h1>
+                        <div className="flex items-center justify-evenly w-full gap-1">
+                            <div className="flex flex-col items-center gap-1 font-bold">
+                                <Star fill="orange" strokeWidth={2} className="text-yellow-100 dark:text-yellow-300" size={40} />
+                                {filteredSortedEmployees?.[0]?.attendance_rating?.good ?? 0}
+                            </div>
+                            <div className="flex flex-col items-center gap-1 font-bold">
+                                <Clock fill="red" strokeWidth={2} className="text-red-100" size={40} />
+                                {filteredSortedEmployees?.[0]?.attendance_rating?.late ?? 0}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center bg-amber-400 dark:bg-sky-900 rounded-t-3xl h-52">
+                        <h1 className="font-black">3rd</h1>
+                        <Medal size={40} fill="yellow" className="dark:text-yellow-600" />
+                        <span className="text-2xl font-bold">{filteredSortedEmployees?.[2]?.attendance_rating?.rating ?? 0}</span>
+                        <h1 className="font-bold bg-amber-600 dark:bg-sky-700 text-xs px-2 rounded-full text-white my-2">
+                            {filteredSortedEmployees?.[2]?.contact?.name ?? ""}
+                        </h1>
+                        <div className="flex items-center justify-evenly w-full gap-1">
+                            <div className="flex flex-col items-center gap-1 font-bold">
+                                <Star fill="orange" strokeWidth={2} className="text-yellow-100 dark:text-yellow-300" size={30} />
+                                {filteredSortedEmployees?.[2]?.attendance_rating?.good ?? 0}
+                            </div>
+                            <div className="flex flex-col items-center gap-1 font-bold">
+                                <Clock fill="red" strokeWidth={2} className="text-red-100" size={30} />
+                                {filteredSortedEmployees?.[2]?.attendance_rating?.late ?? 0}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AttendanceSummaryList;
